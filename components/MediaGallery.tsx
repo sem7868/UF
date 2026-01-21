abf91adf142a94e2546ae61a26080123cf313e87
sem7868 <sem7868@gmail.com>
2026-01-21 17:44:13 +0530

    Update MediaGallery.tsx

--- components/MediaGallery.tsx
+++ components/MediaGallery.tsx
@@ -1,1 +1,12 @@
-
+export default function MediaGallery({ images, videos }) {
+  return (
+    <>
+      {images.map((img, i) => (
+        <img key={i} src={img} width="100%" />
+      ))}
+      {videos.map((vid, i) => (
+        <video key={i} src={vid} controls width="100%" />
+      ))}
+    </>
+  );
+}
