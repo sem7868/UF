6d4aeb05aa12c5c3fb37cc140403358f20b6f2cf
sem7868 <sem7868@gmail.com>
2026-01-21 17:46:48 +0530

    Update data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,1 +1,15 @@
+export const products = [
+  {
+    id: "mul-chanderi",
+    name: "Mul Chanderi Dyeable",
+    moq: 50,
+    images: ["/placeholder.jpg"],
+    videos: [],
+    slabs: [
+      { min: 50, max: 99, price: 145 },
+      { min: 100, max: 199, price: 138 },
+      { min: 200, max: null, price: 130 }
+    ]
+  }
+];
 
